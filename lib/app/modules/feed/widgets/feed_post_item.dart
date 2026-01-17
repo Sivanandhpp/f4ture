86046305dd53b5c200a3352adbd2c4beb309f35d
Sivanandh PP <sivanandhpp@gmail.com>
2026-01-17 16:57:50 +0530

    double tap to like

--- lib/app/modules/feed/widgets/feed_post_item.dart
+++ lib/app/modules/feed/widgets/feed_post_item.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:timeago/timeago.dart' as timeago;
@@ -7,15 +8,40 @@ import '../../../data/models/post_model.dart';
 import '../controllers/feed_controller.dart';
 import 'media_carousel.dart';
 
-class FeedPostItem extends StatelessWidget {
+class FeedPostItem extends StatefulWidget {
   final PostModel post;
-  final FeedController controller = Get.find();
 
-  FeedPostItem({super.key, required this.post});
+  const FeedPostItem({super.key, required this.post});
+
+  @override
+  State<FeedPostItem> createState() => _FeedPostItemState();
+}
+
+class _FeedPostItemState extends State<FeedPostItem> {
+  final FeedController controller = Get.find();
+  bool isHeartAnimating = false;
 
   static const Color kSurface = Color(0xFF1E1E1E);
   static const Color kTextPrimary = Colors.white;
   static const Color kTextSecondary = Color(0xFFAAAAAA);
+
+  void _handleDoubleTap() {
+    setState(() {
+      isHeartAnimating = true;
+    });
+
+    if (!widget.post.isLikedByMe) {
+      controller.toggleLike(widget.post);
+    }
+
+    Timer(const Duration(milliseconds: 1200), () {
+      if (mounted) {
+        setState(() {
+          isHeartAnimating = false;
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +70,7 @@ class FeedPostItem extends StatelessWidget {
               children: [
                 CircleAvatar(
                   radius: 18,
-                  backgroundImage: NetworkImage(post.authorAvatar),
+                  backgroundImage: NetworkImage(widget.post.authorAvatar),
                   backgroundColor: Colors.grey[800],
                   onBackgroundImageError: (_, __) =>
                       const Icon(Icons.person, color: Colors.white),
@@ -55,7 +81,7 @@ class FeedPostItem extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        post.authorName,
+                        widget.post.authorName,
                         style: const TextStyle(
                           color: kTextPrimary,
                           fontWeight: FontWeight.bold,
@@ -65,13 +91,13 @@ class FeedPostItem extends StatelessWidget {
                       Row(
                         children: [
                           Text(
-                            timeago.format(post.createdAt),
+                            timeago.format(widget.post.createdAt),
                             style: const TextStyle(
                               color: kTextSecondary,
                               fontSize: 11,
                             ),
                           ),
-                          if (post.type == PostType.blog) ...[
+                          if (widget.post.type == PostType.blog) ...[
                             const SizedBox(width: 8),
                             Container(
                               padding: const EdgeInsets.symmetric(
@@ -110,11 +136,11 @@ class FeedPostItem extends StatelessWidget {
           const SizedBox(height: 12),
 
           // 2. Caption (Top)
-          if (post.text.isNotEmpty)
+          if (widget.post.text.isNotEmpty)
             Padding(
               padding: const EdgeInsets.only(bottom: 12.0, left: 16, right: 16),
               child: Text(
-                post.text,
+                widget.post.text,
                 style: const TextStyle(
                   color: kTextPrimary,
                   fontSize: 14,
@@ -124,19 +150,43 @@ class FeedPostItem extends StatelessWidget {
             ),
 
           // 3. Media (Rounded & Bordered)
-          if (post.mediaUrls.isNotEmpty)
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                border: Border.all(
-                  color: AppColors.primary.withOpacity(0.3),
-                  width: 1,
-                ),
-              ),
-              clipBehavior: Clip.antiAlias,
-              child: MediaCarousel(
-                mediaUrls: post.mediaUrls,
-                thumbnailUrl: post.thumbnailUrl,
+          if (widget.post.mediaUrls.isNotEmpty)
+            GestureDetector(
+              onDoubleTap: _handleDoubleTap,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(
+                        color: AppColors.primary.withOpacity(0.3),
+                        width: 1,
+                      ),
+                    ),
+                    clipBehavior: Clip.antiAlias,
+                    child: MediaCarousel(
+                      mediaUrls: widget.post.mediaUrls,
+                      thumbnailUrl: widget.post.thumbnailUrl,
+                    ),
+                  ),
+                  if (isHeartAnimating)
+                    TweenAnimationBuilder<double>(
+                      tween: Tween(begin: 0.0, end: 1.0),
+                      duration: const Duration(milliseconds: 200),
+                      curve: Curves.elasticOut,
+                      builder: (context, value, child) {
+                        return Transform.scale(
+                          scale: value,
+                          child: const Icon(
+                            Icons.favorite,
+                            color: Colors.white,
+                            size: 100,
+                          ),
+                        );
+                      },
+                    ),
+                ],
               ),
             ),
 
@@ -148,29 +198,23 @@ class FeedPostItem extends StatelessWidget {
             child: Row(
               children: [
                 _buildInteractionButton(
-                  icon: post.isLikedByMe
+                  icon: widget.post.isLikedByMe
                       ? Icons.favorite
                       : Icons.favorite_border,
-                  color: post.isLikedByMe ? Colors.red : kTextPrimary,
-                  label: '${post.likesCount}',
-                  onTap: () => controller.toggleLike(post),
+                  color: widget.post.isLikedByMe ? Colors.red : kTextPrimary,
+                  label: '${widget.post.likesCount}',
+                  onTap: () => controller.toggleLike(widget.post),
                 ),
                 const SizedBox(width: 20),
                 _buildInteractionButton(
                   icon: Icons.chat_bubble_outline_rounded,
                   color: kTextPrimary,
-                  label: '${post.commentsCount}',
-                  onTap: () => controller.openComments(post),
+                  label: '${widget.post.commentsCount}',
+                  onTap: () => controller.openComments(widget.post),
                 ),
                 const SizedBox(width: 20),
                 // Share (Placeholder)
-                // const Icon(Icons.send_rounded, color: kTextPrimary, size: 22),
                 const Spacer(),
-                // Icon(
-                //   Icons.bookmark_border_rounded,
-                //   color: kTextPrimary,
-                //   size: 24,
-                // ),
               ],
             ),
           ),
